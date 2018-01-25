552d6b0cbebcc1f9375c1358c25f97b7b3f44550
QiuShuiBai <1125097005@qq.com>
2018-01-26 01:25:32 +0800

    杨辉三角

---
+++ 杨辉三角.js
@@ -0,0 +1,16 @@
+var i = 1;
+var a = [[1], [1, 1]];
+console.log(a[0]);
+var q = 0;
+while (i < 5) {
+	q = i + 1;          
+	while (i - 1) {
+		a[1][0] = 1;
+		a[1][q - i] = a[0][q - i - 1] + a[0][q - i];
+		i--;
+	}
+	a[1][q - 1] = 1;
+	a[0] = [...a[1]];
+	console.log(a[0]);
+	i = q;
+}
