8e47cab2a0fb4c475a06a0d730a920251635eccc
QiuShuiBai <1125097005@qq.com>
2019-03-19 11:21:55 +0800

    [feat]baseFun: 添加基础函数

--- baseFun.js
+++ baseFun.js
@@ -26,6 +26,39 @@ function numCount(arr, num) {
   })
   return count
 }
+
+// 交换数组两个值
+function swap(arr, i, j) {
+  [arr[i], arr[j]] = [arr[j], arr[i]]
+}
+
+// 选择数组中的一个值，取出后插入至选定的索引，该索引之后的值的索引加一
+// [1, 2, 3, 4]  =>  [4, 1, 2, 3]
+function insert(arr, i, j) {
+  let item = arr.splice(j, 1)[0]
+  arr.splice(i, 0, item)
+}
+
+function arrMin(arr) {
+  let min = arr[0]
+  arr.forEach(item => {
+    min = Math.min(min, item)
+  })
+  return min
+}
+
+function arrMax(arr) {
+  let min = arr[0]
+  arr.forEach(item => {
+    min = Math.max(min, item)
+  })
+  return min
+}
+
 module.exports.createArr = createArr
 module.exports.shuffle = shuffle
 module.exports.numCount = numCount
+module.exports.swap = swap
+module.exports.insert = insert
+module.exports.arrMin = arrMin
+module.exports.arrMax = arrMax
